ad69ec5bb2c87b8c2e6caad4831871c8f65241a9
Ivan Trofimov <richard1997dog@gmail.com>
2017-11-09 10:27:05 +0300

    Initial Commit

---
+++ HRHack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HRHack
+//
+//  Created by Ivan Trofimov on 09/11/2017.
+//  Copyright © 2017 Ivan Trofimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
